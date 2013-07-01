4ed984677c52ad67e1d6a18a53759086001cea5c
Ivan Kuzmin <inkuzmin@ya.ru>
2013-07-02 04:46:58 +1100

    Add loading indicator and progress control

--- js/ascii-player.js
+++ js/ascii-player.js
@@ -28,6 +28,7 @@ A.prototype = {
         self._initAudio();
         self._render();
         self._getElements();
+        self._addPlayer();
         self._bindHandlers();
 
         self._setVolumeLevel(0.75);
@@ -43,9 +44,9 @@ A.prototype = {
     _initAudio: function () {
         var self = this;
         self.audio = new Audio();
-        self.audio.setAttribute('src', self.src);
+        self.audio.setAttribute('src', self.src + '?rand=' + Math.random());
 //        self.audio.setAttribute('type', 'audio/mpeg');
-        self.audio.setAttribute('preload', true);
+        self.audio.setAttribute('preload', 'none');
 //        self.audio.load();
     },
     _render: function () {
@@ -53,23 +54,23 @@ A.prototype = {
         var templ = '<div class="player" id="player' + self.id + '">' +
                     '   <div class="ctrl play-btn" id="play' + self.id + '">' +
                     '       <div>' + A.S.l + '</div>' +
-                    '       <div id="play-symbol' + self.id + '">' + A.S.p + '</div>' +
+                    '       <div class="play" id="play-symbol' + self.id + '">' + A.S.p + '</div>' +
                     '       <div>' + A.S.r + '</div>' +
                     '   </div>' +
                     '   <div class="separator">' + A.S.s + '</div>' +
                     '   <div class="ctrl progress">' +
                     '       <div>' + A.S.l + '</div>' +
                     '       <div id="progress' + self.id + '">' + self._renderProgress(20) + '</div>' +
+                    '       <div id="time' + self.id + '">00:00</div>' +
                     '       <div>' + A.S.r + '</div>' +
-                    '       <div id="time' + self.id + '">88:88</div>' +
                     '   </div>' +
                     '   <div class="separator">' + A.S.s + '</div>' +
                     '   <div class="tip">Vol.:</div>' +
                     '   <div class="ctrl volume">' +
                     '       <div>' + A.S.l + '</div>' +
                     '       <div id="volume' + self.id + '">' + self._renderVolume(5) + '</div>' +
-                    '       <div>' + A.S.r + '</div>' +
                     '       <div id="level' + self.id + '">88%</div>' +
+                    '       <div>' + A.S.r + '</div>' +
                     '   </div>' +
                     '   <div class="separator">' + A.S.s + '</div>' +
                     '   <div class="tip">' +
@@ -105,7 +106,7 @@ A.prototype = {
         var num = Math.floor((length-1)*volumeLevel);
 
         vols[num].innerHTML = A.S.o;
-        vols[num].className = 'vol active';
+        self._addClass(vols[num], 'active');
     },
 
     _setProgressPosition: function (secs) {
@@ -121,14 +122,16 @@ A.prototype = {
         var length = self.progs.length;
         var progs = self.progs.nodes;
 
-        var num = Math.floor(((length-1)*secs)/self.audio.duration);
+        var num = Math.floor(((length)*secs)/self.audio.duration);
 
         for (;length--;) {
             progs[length].innerHTML = A.S.u;
-            progs[length].className = 'prog';
+            self._removeClass(progs[length], 'active');
+
         }
         progs[num].innerHTML = A.S.o;
-        progs[num].className = 'prog active';
+        self._addClass(progs[num], 'active');
+
     },
     _getElements: function () {
         var self = this,
@@ -142,10 +145,13 @@ A.prototype = {
         self.progressNode = d.getElementById('progress' + self.id);
         self.volumeNode = d.getElementById('volume' + self.id);
 
-//        self.playerNode.appendChild(self.audio);
-
         self._getVols();
         self._getProgs();
+    },
+    _addPlayer: function () {
+        var self = this;
+        self.playerNode.appendChild(self.audio);
+//        self.audio.load();
     },
     _getVols: function () {
         var self = this;
@@ -181,15 +187,48 @@ A.prototype = {
         }, false);
 
 
-        self.audio.addEventListener('durationchange', function () {
-            self._setTime();
+        self.audio.addEventListener('canplay', function () {
+//            self._play();
+//            self._play();
+            self._bufferisation();
         }, false);
         self.audio.addEventListener('timeupdate', function () {
             self._setTime();
             self._setProgressPosition(self.audio.currentTime);
         }, false);
 
+        self.audio.addEventListener('pause', function () {
+//            self._play();
+        }, false);
 
+
+    },
+    _bufferisation: function () {
+        var self = this;
+        var interval = setInterval(function () {
+            if (self._wasBuffered()) {
+                clearInterval(interval);
+            }
+            self._paintProgressBar();
+        }, 1000);
+
+    },
+    _wasBuffered: function () {
+        var self = this;
+        return self.audio.buffered.end(0) === self.audio.duration;
+    },
+    _paintProgressBar: function () {
+        var self = this;
+        var d = self.audio.duration,
+            b = self.audio.buffered.end(0),
+            p = b / d,
+            l = self.progs.length,
+            c = Math.floor(l*p),
+            i;
+        for (i = 0; i < c; i += 1) {
+            self._addClass(self.progs.nodes[i], 'buff');
+        }
+        self.buff = c;
     },
     _changeVolume: function (clickedNode) {
         var self = this;
@@ -198,34 +237,37 @@ A.prototype = {
 
         var unit = 1/(length-1);
         for (var i = 0; i < length; i += 1) {
-            if (vols[i].className === 'vol active') {
+            if (self._hasClass(vols[i], 'active')) {
                 vols[i].innerHTML = A.S.u;
-                vols[i].className = 'vol';
+                self._removeClass(vols[i], 'active');
             }
             if (vols[i] === clickedNode) {
-                vols[i].className = 'vol active';
-
+                self._addClass(vols[i], 'active');
                 self._setVolumeLevel(i*unit);
             }
         }
     },
     _changeProgress: function (clickedNode) {
         var self = this;
-//        var length = self.vols.length;
-//        var vols = self.vols.nodes;
-//
-//        var unit = 1/(length-1);
-//        for (var i = 0; i < length; i += 1) {
-//            if (vols[i].className === 'vol active') {
-//                vols[i].innerHTML = A.S.u;
-//                vols[i].className = 'vol';
-//            }
-//            if (vols[i] === clickedNode) {
-//                vols[i].className = 'vol active';
-//
-//                self._setVolumeLevel(i*unit);
-//            }
-//        }
+        var length = self.progs.length;
+        var progs = self.progs.nodes;
+        var current;
+
+        var unit = self.audio.duration/(length);
+        for (var i = 0; i < length; i += 1) {
+            if (progs[i] === clickedNode) {
+                if (i > self.buff) {
+                    return;
+                }
+                else {
+                    current = i;
+                    self._addClass(progs[i], 'active');
+                    self.audio.currentTime = i*unit + 0.1;
+                }
+            }
+
+
+        }
     },
     _play: function () {
         var self = this;
@@ -272,6 +314,25 @@ A.prototype = {
     _replaceElement: function (oldEl, newEl) {
         var self = this;
         oldEl.parentNode.replaceChild(newEl, oldEl);
+    },
+    _addClass: function (el, cl) {
+        el.className += ' ' + cl;
+    },
+    _removeClass: function (el, cl) {
+        var i, r = [],
+            c = el.className.split(' ');
+        for (i = 0; i < c.length; i += 1) {
+            if (c[i] !== cl) {
+                r.push(c[i]);
+            }
+        }
+        el.className = r.join(' ');
+    },
+    _hasClass: function (el, cl) {
+        if (el.className.indexOf(cl) > -1) {
+            return true;
+        }
+        return false;
     }
 };
 
